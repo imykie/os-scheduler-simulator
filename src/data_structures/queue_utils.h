f9dbeddf0572da633ee52e24efa53bbd4a90d5c9
Michael Olatunji <tundexmike@gmail.com>
2024-02-12 22:42:51 +0100

    chore: make queue utils functions inline

--- src/data_structures/queue_utils.h
+++ src/data_structures/queue_utils.h
@@ -7,11 +7,27 @@
 
 namespace QueueUtils {
     template <typename T>
-    std::vector<T> ConvertToVector(Queue<T>* queue);
+    std::vector<T> ConvertToVector(Queue<T>* queue) {
+        std::vector<T> vector;
+        for (int i = 0; i < queue->Length(); ++i) {
+            T item = queue->Dequeue();
+            vector.push_back(item);
+            queue->Enqueue(item);
+        }
+        return vector;
+    }
 
     template <typename T>
-    Queue<T>* ConvertFromVector(std::vector<T> vector);
+    Queue<T>* ConvertFromVector(std::vector<T> vector) {
+        Queue<T>* queue = new Queue<T>();
+        for (T item : vector) queue->Enqueue(item);
+        return queue;
+    }
 
     template <typename T>
-    Queue<T>* Sort(Queue<T>* queue, bool (*sort_function)(T, T));
+    Queue<T>* Sort(Queue<T>* queue, bool (*sort_function)(T, T)) {
+        std::vector<T> vector = ConvertToVector(queue);
+        std::sort(vector.begin(), vector.end(), sort_function);
+        return ConvertFromVector(vector);
+    }
 }  // namespace QueueUtils
